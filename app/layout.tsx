5a411ed8a029b38fb983d7d67f4ebf8e67af6c6a
Admiral-Simo <admiralsimo16@gmail.com>
2025-09-20 21:17:38 +0100

    fix the og.png bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,20 +51,13 @@ export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
+    images: [],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [siteConfig.ogImage],
+    images: [],
     creator: "@cypherab01",
   },
   icons: {
